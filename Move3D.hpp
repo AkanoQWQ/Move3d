67c3ee381f3d41957f7ef209f634fbd7906c5724
Akano <61079646+AkanoQWQ@users.noreply.github.com>
2026-01-10 20:25:00 +0800

    Update Point and ULM

--- Move3D.hpp
+++ Move3D.hpp
@@ -1,14 +1,22 @@
 namespace Move3D{
-    struct Point{
-        double x,y,z;
-    };
     struct Vector{
         double x,y,z;
+        Vector() = default;
         Vector(double _x,double _y,double _z){
             x = _x,y = _y,z = _z;
         }
         Vector operator*(double ratio){
             return Vector(x * ratio,y * ratio,z * ratio);
+        }
+    };
+    struct Point{
+        double x,y,z;
+        Point() = default;
+        Point(double _x,double _y,double _z){
+            x = _x,y = _y,z = _z;
+        }
+        Point operator+(Vector delta){
+            return Point(this->x + delta.x,this->y + delta.y,this->z + delta.z);
         }
     };
 
@@ -19,5 +27,17 @@ namespace Move3D{
         virtual ~Movement() = default;
         virtual Point Move(double dt) = 0;
     };
-    
+
+    class UniformLinearMotion : Move3D::Movement{
+    private:
+        Vector velocity;
+    public:
+        UniformLinearMotion(Point _nowPosition,Vector _velocity){
+            nowPosition = _nowPosition;
+            velocity = _velocity;
+        }
+        Move3D::Point Move(double dt) override {
+            return nowPosition + velocity * dt;
+        }
+    };
 }//namespace Move3D
