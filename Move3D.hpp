03565fff5651062b5d278351282438828cf729e4
Akano <61079646+AkanoQWQ@users.noreply.github.com>
2026-01-10 20:20:08 +0800

    Update operator* for vector

--- Move3D.hpp
+++ Move3D.hpp
@@ -1,9 +1,19 @@
 namespace Move3D{
-    struct Point{double x,y,z;};
-    struct Vector{double x,y,z;};
+    struct Point{
+        double x,y,z;
+    };
+    struct Vector{
+        double x,y,z;
+        Vector(double _x,double _y,double _z){
+            x = _x,y = _y,z = _z;
+        }
+        Vector operator*(double ratio){
+            return Vector(x * ratio,y * ratio,z * ratio);
+        }
+    };
 
     class Movement{
-    private:
+    protected:
         Point nowPosition;
     public:
         virtual ~Movement() = default;
